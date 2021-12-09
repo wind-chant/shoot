78c100ed12c660b17b430b51d12ea552af7b3dc5
明豪 陈 <1517286910@qq.com>
2021-12-09 23:01:58 +0800

    创建三层架构文件夹

---
+++ shootModels/Base.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shootModels
+{
+    /// <summary>
+    /// 所有物品的基类
+    /// </summary>
+    abstract class Base
+    {
+        // 位置
+        private Point location;
+        
+        // 是否存在
+        private bool live;
+
+        public Point Location
+        {
+            set { location = value; }
+        }
+
+        public bool Live
+        {
+            get { return live; }
+            set { live = value; }
+        }
+
+        public int X
+        {
+            get { return location.X; }
+        }
+
+        public int Y
+        {
+            get { return location.Y; }
+        }
+
+        /// <summary>
+        /// 绘图方法
+        /// </summary>
+        /// <param name="g"></param>
+        public abstract void Draw(Graphics g);
+    }
+}
